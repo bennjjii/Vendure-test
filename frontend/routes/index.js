4b7356ac457472adfb6b699b3b4727c5ac31f737
bravis85 <73161499+bravis85@users.noreply.github.com>
2021-01-14 22:29:00 +0000

    Started linking frontend to backend. First graphQL query working

--- frontend/routes/index.js
+++ frontend/routes/index.js
@@ -1,9 +1,32 @@
-var express = require('express');
+const { default: axios } = require("axios");
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  axios
+    .post("http://localhost:3000/shop-api", {
+      query: "{products{items{name,id,description}}}",
+    })
+    .then((res) => {
+      return res.data.data.products.items;
+    })
+    .then((data) => {
+      console.log(data);
+      res.render("index", { title: "Express", productList: data });
+    });
+});
+
+/* GET shop page */
+
+router.get("/shop", (req, res, next) => {
+  res.send(200);
+});
+
+/* GET detail page */
+
+router.get("/detail/:id", (req, res, next) => {
+  res.status(200).send(req.params.id);
 });
 
 module.exports = router;
